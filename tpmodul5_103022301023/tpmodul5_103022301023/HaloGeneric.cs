9fe5ea49dddb261e84d4c5e800db206338cb77ea
raatra10 <rafi180350@gmail.com>
2025-03-17 00:33:57 +0700

    Versi awal project

---
+++ tpmodul5_103022301023/tpmodul5_103022301023/HaloGeneric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace tpmodul5_103022301023
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+}
+
--- tpmodul5_103022301023/tpmodul5_103022301023/Program.cs
+++ tpmodul5_103022301023/tpmodul5_103022301023/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using System;
+namespace tpmodul5_103022301023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+            Console.Write("Masukkan nama: ");
+            string nama = Console.ReadLine();
+            halo.SapaUser(nama);
+            Console.ReadLine();
+        }
+    }
+}
